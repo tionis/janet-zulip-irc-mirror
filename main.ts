02e8fac7f4dd89d04e9f5aedd37ace69a898f7dc
tionis <git@tionis.dev>
2024-10-12 00:37:10 +0200

    updating to version from daemons

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
-#!/bin/env -S deno run --allow-net=janet.zulipchat.com:443,irc.libera.chat:6697 --allow-env=JANET_ZULIP_IRC_BRIDGE_ZULIP_USERNAME,JANET_ZULIP_IRC_BRIDGE_ZULIP_KEY,JANET_ZULIP_IRC_BRIDGE_IRC_PASSWORD --env
-import { Client as IrcClient } from "https://deno.land/x/irc/mod.ts";
+#!/bin/env -S deno run --unstable-cron --allow-net=janet.zulipchat.com:443,irc.libera.chat:6697,ntfy.tionis.dev:443 --allow-env --env
+import { Client as IrcClient } from "https://deno.land/x/irc@v0.15.0/mod.ts";
+import LeakyBucket from "./LeakyBucket.js";
+
+const version = "0.1.0";
 
 // Types
 type IntToStr = { [key: number]: string };
@@ -7,16 +10,46 @@ type StrToStr = { [key: string]: string };
 type StrToInt = { [key: string]: number };
 
 // Static Config[]
+const NTFY_TOKEN: string = Deno.env.get("NTFY_TOKEN") as string;
+const PING_TOKEN: string = Deno.env.get("PING_TOKEN") as string;
 const zulipUsername: string =
   Deno.env.get("JANET_ZULIP_IRC_BRIDGE_ZULIP_USERNAME") ?? "";
 const zulipKey: string = Deno.env.get("JANET_ZULIP_IRC_BRIDGE_ZULIP_KEY") ?? "";
 const zulipAuthHeader: string = "Basic " + btoa(zulipUsername + ":" + zulipKey);
-const ircAdmins: string[] = ["tionis"];
+const ircAdmins: Set<string> = new Set(["tionis"]);
 if (zulipUsername === "" || zulipKey === "") {
   console.error(
     "[ERROR] Zulip username or key not set in environment variables.",
   );
   Deno.exit(1);
+}
+
+async function ntfy(channel: string, message: string) {
+  const result = await fetch(
+    `https://ntfy.tionis.dev/janet-zulip-irc-bridge/${channel}`,
+    {
+      method: "POST",
+      headers: { Authorization: NTFY_TOKEN },
+      body: message,
+    },
+  );
+  if (!result.ok) {
+    console.error(`Failed to send notification: ${result.statusText}`);
+  }
+}
+
+async function ntfy_json(channel: string, message: unknown) {
+  const result = await fetch(
+    `https://ntfy.tionis.dev/janet-zulip-irc-bridge/${channel}?format=json`,
+    {
+      method: "POST",
+      headers: { Authorization: NTFY_TOKEN },
+      body: JSON.stringify(message),
+    },
+  );
+  if (!result.ok) {
+    console.error(`Failed to send notification: ${result.statusText}`);
+  }
 }
 
 // Helper functions
@@ -33,6 +66,16 @@ async function zulipGetQueue() {
   });
   const text = await resp.text();
   return JSON.parse(text).queue_id;
+}
+
+const bucket = new LeakyBucket({
+  // TODO this is broken atm
+  capacity: 5,
+  interval: 3,
+});
+async function privmsg(target: string, message: string) {
+  await bucket.throttle();
+  irc.privmsg(target, message);
 }
 
 // Dynamic Config
@@ -74,12 +117,15 @@ for (const [zulipID, ircChannel] of Object.entries(zulipID_to_IrcChannel)) {
 const irc = new IrcClient({
   nick: "janet-zulip",
   authMethod: "sasl",
-  reconnect: { attempts: -1, delay: 10, exponentialBackoff: true },
-  channels: Object.values(zulipID_to_IrcChannel),
+  reconnect: { attempts: -1, delay: 10 }, //, exponentialBackoff: true },
+  channels: [
+    Object.values(zulipID_to_IrcChannel)[0],
+    ...Object.values(zulipID_to_IrcChannel).slice(1),
+  ],
   password: Deno.env.get("JANET_ZULIP_IRC_BRIDGE_IRC_PASSWORD")!,
 });
 
-let last_hearbeat_time: Date;
+let last_heartbeat_time: Date;
 
 irc.on("privmsg:channel", ({ source, params }) => {
   console.log(
@@ -178,33 +224,37 @@ irc.on("myinfo", (msg) => {
 });
 
 irc.on("error", (error) => {
+  ntfy_json("errors/irc", {
+    name: error.name,
+    message: error.message,
+    type: error.type.toString(),
+  });
   console.log(
     `[ERROR] <irc> Name: ${error.name}\n[^^^^^] <irc> Message: ${error.message}\n[^^^^^] <irc> Type: ${error.type}`,
   );
-  // TODO send admin notification (over some other channel)
 });
 
 irc.on("privmsg:private", ({ source, params }) => {
   console.log(`[PRIVMSG] <irc> ${source?.name}: ${params.text}`);
-  const is_admin: boolean = ircAdmins.includes(source?.name!);
+  const is_admin: boolean = ircAdmins.has(source?.name!);
   const commands: string[] = is_admin
-    ? ["heartbeat", "ping", "help"]
-    : ["heartbeat", "ping", "help", "msg", "join", "part"];
+    ? ["heartbeat", "ping", "help", "msg", "join", "part", "ntfy"]
+    : ["heartbeat", "ping", "help"];
   const command = params.text.split(" ")[0];
   switch (command) {
     case "heartbeat":
-      irc.privmsg(source?.name!, last_hearbeat_time.toString());
+      privmsg(source?.name!, last_heartbeat_time.toString());
       break;
     case "ping":
-      irc.privmsg(source?.name!, "pong");
+      privmsg(source?.name!, "pong");
       break;
     case "help":
-      irc.privmsg(source?.name!, `Commands: ${commands.join(", ")}`);
+      privmsg(source?.name!, `Commands: ${commands.join(", ")}`);
       break;
     case "msg":
       if (is_admin) {
         const [_, target, ...message] = params.text.split(" ");
-        irc.privmsg(target, message.join(" "));
+        privmsg(target, message.join(" "));
       }
       break;
     case "join":
@@ -219,8 +269,17 @@ irc.on("privmsg:private", ({ source, params }) => {
         irc.part(channel);
       }
       break;
+    case "ntfy":
+      if (is_admin) {
+        const [_, ...message] = params.text.split(" ");
+        ntfy("irc/dm", message.join(" "));
+      }
+      break;
+    case "version":
+      privmsg(source?.name!, version);
+      break;
     default:
-      irc.privmsg(
+      privmsg(
         source?.name!,
         `Unknown command. Commands: ${commands.join(", ")}`,
       );
@@ -229,8 +288,21 @@ irc.on("privmsg:private", ({ source, params }) => {
 
 irc.connect("irc.libera.chat", 6697, true);
 
+async function deadManPing() {
+  const result = await fetch("https://cloud.tionis.dev/public.php/webdav/", {
+    method: "PUT",
+    headers: { Authorization: "Basic " + btoa(PING_TOKEN + ":") },
+    body: new Date().getTime().toString(),
+  });
+  if (!result.ok) {
+    console.error(`Failed to send ping: ${result.statusText}`);
+  }
+}
+Deno.cron("deadManPing", "*/5 * * * *", deadManPing);
+
 console.log("[INFO] Starting zulip event loop...");
 (async () => {
+  let failedPulls = 0;
   while (true) {
     try {
       const params = new URLSearchParams({
@@ -242,68 +314,87 @@ console.log("[INFO] Starting zulip event loop...");
         method: "GET",
         headers: { Authorization: zulipAuthHeader },
       });
-      const parsedResp = JSON.parse(await resp.text());
-      if (parsedResp.result != "success") {
-        switch (parsedResp.code) {
-          case "BAD_EVENT_QUEUE_ID":
-            console.log(
-              "[INFO] <zulip> Event queue id expired. Requesting new queue id...",
-            );
-            queue_id = await zulipGetQueue();
-            last_event_id = -1;
-            continue;
-          default:
-            console.log("[ERROR] <zulip> Unknown error occurred:");
-            console.log(parsedResp);
-            for (const admin of ircAdmins) {
-              irc.privmsg(admin, `Error fetching events from zulip api:`);
-              irc.privmsg(admin, JSON.stringify(parsedResp));
-            }
-            break;
-        }
-        return;
-      }
-      for (const event of parsedResp.events) {
-        if (event.type === "heartbeat") {
-          last_hearbeat_time = new Date();
-        } else if (
-          event.type === "message" &&
-          event.message.sender_email !== zulipUsername
-        ) {
-          console.log(["DEBUG", "New Message", event]);
-          if (event.message.type === "stream") {
-            console.log(
-              `[INFO] <zulip> ${event.message.sender_full_name}(${event.message.subject})@${event.message.display_recipient}: ${event.message.content}`,
-            );
-            const irc_channel = zulipID_to_IrcChannel[event.message.stream_id];
-            const lines = event.message.content.trim().split("\n");
-            const prefix = `${event.message.sender_full_name}(${event.message.subject}):`;
-            console.log([
-              "sending_irc_messages",
-              {
-                event: event,
-                lines: lines,
-              },
-            ]);
-            for (const line of lines) {
-              irc.privmsg(irc_channel, `${prefix} ${line}`);
-            }
-          } else {
-            console.log(
-              event.message.sender_email + ": " + event.message.content,
-            );
+      if (resp.headers.get("content-type") === "application/json") {
+        const parsedResp = JSON.parse(await resp.text());
+        if (parsedResp.result != "success") {
+          switch (parsedResp.code) {
+            case "BAD_EVENT_QUEUE_ID":
+              console.log(
+                "[INFO] <zulip> Event queue id expired. Requesting new queue id...",
+              );
+              queue_id = await zulipGetQueue();
+              last_event_id = -1;
+              continue;
+            default:
+              console.log("[ERROR] <zulip> Unknown error occurred:");
+              console.log(parsedResp);
+              for (const admin of ircAdmins) {
+                privmsg(admin, `Error fetching events from zulip api:`);
+                privmsg(admin, JSON.stringify(parsedResp));
+              }
+              break;
           }
+          return;
         }
-        last_event_id = event.id;
+        for (const event of parsedResp.events) {
+          failedPulls = 0;
+          if (event.type === "heartbeat") {
+            last_heartbeat_time = new Date();
+          } else if (
+            event.type === "message" &&
+            event.message.sender_email !== zulipUsername
+          ) {
+            //console.log(["DEBUG", "New Message", event]);
+            if (event.message.type === "stream") {
+              console.log(
+                `[INFO] <zulip> ${event.message.sender_full_name}(${event.message.subject})@${event.message.display_recipient}: ${event.message.content}`,
+              );
+              const irc_channel =
+                zulipID_to_IrcChannel[event.message.stream_id];
+              const lines = event.message.content.trim().split("\n");
+              //const prefix = `${event.message.sender_full_name}(${event.message.subject}):`;
+              console.log([
+                "sending_irc_messages",
+                {
+                  event: event,
+                  lines: lines,
+                },
+              ]);
+              privmsg(
+                irc_channel,
+                `${event.message.sender_full_name}@[${event.message.subject}]:`,
+              );
+              for (const line of lines) {
+                privmsg(irc_channel, line);
+                //privmsg(irc_channel, `${prefix} ${line}`);
+              }
+            } else {
+              console.log(
+                event.message.sender_email + ": " + event.message.content,
+              );
+            }
+          }
+          last_event_id = event.id;
+        }
+      } else {
+        failedPulls++;
       }
     } catch (e) {
       console.log(`[ERROR] <zulip> Error fetching events from zulip api: ${e}`);
       for (const admin of ircAdmins) {
-        irc.privmsg(admin, `Error fetching events from zulip api:`);
-        irc.privmsg(admin, e.toString());
+        privmsg(admin, `Error fetching events from zulip api:`);
+        privmsg(admin, e.toString());
       }
       // wait for 10 seconds before retrying
       await new Promise((resolve) => setTimeout(resolve, 10000));
+    }
+    if (failedPulls >= 5) {
+      ntfy(
+        "errors/zulip",
+        "Failed to fetch events from zulip api 5 times in a row. Waiting for 1 minute before retrying.",
+      );
+      failedPulls = 0;
+      await new Promise((resolve) => setTimeout(resolve, 60000));
     }
   }
 })();
